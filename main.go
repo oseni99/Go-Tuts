cd6f052bd4b0603f5b0ae0a2e5494cc76770647f
Oluwatosin <kingosenitosin@gmail.com>
2024-11-20 00:51:03 -0600

    added helper package plus more tuts

--- main.go
+++ main.go
@@ -1,55 +1,43 @@
 package main
 
 import (
+	"booking-app/helper"
 	"fmt"
-	"strings"
 )
 
+// := does not work for global variables in Go i.e packages
+var conferenceName = "Go Conference"
+
+const conferenceTickets int = 50
+
+var remainingTickets uint = 50
+var bookings = make([]UserData, 0)
+
+// struct will give you a predefined structure
+//
+//	use struct if you know the values in the dicts but map if unknown
+type UserData struct {
+	firstName       string
+	lastName        string
+	emailAddress    string
+	numberOfTickets uint
+}
+
 func main() {
-	conferenceName := "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	bookings := []string{}
 
-	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
-
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	greetUsers()
 
 	for {
-		var firstName string
-		var lastName string
-		var emailAddress string
-		var userTickets uint
-		// ask the user for their name
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
-		fmt.Println("Enter your last name:")
-		fmt.Scan(&lastName)
-		fmt.Printf("Enter your email address: ")
-		fmt.Scan(&emailAddress)
-		fmt.Println("Enter number of Tickets: ")
-		fmt.Scan(&userTickets)
 
-		isValidName := len(firstName) >= 3 && len(lastName) >= 3
-		isValidEmail := strings.Contains(emailAddress, "@")
-		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		firstName, lastName, emailAddress, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, emailAddress, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName)
+			bookTicket(userTickets, firstName, lastName, emailAddress)
 
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, emailAddress)
-			fmt.Printf("%v tickets remaining for the %v are left\n", remainingTickets, conferenceName)
-
-			firstNames := []string{}
-			for _, booking := range bookings {
-				var names = strings.Fields(booking) // split the names in that array/slice
-				// var firstName = names[0]
-				firstNames = append(firstNames, names[0])
-			}
-			fmt.Printf("The first names of bookings are: %v", firstNames)
+			//  call func to print first names
+			firstNames := FirstNames()
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
 			if remainingTickets == 0 {
 				// end the program
@@ -67,5 +55,65 @@ func main() {
 		}
 
 	}
+}
 
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+// In Go i have to specify the exact type of what im returning when i use return statement
+func FirstNames() []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+	return firstNames
+}
+
+//  You can return multiple functions in go which is super amazing
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var emailAddress string
+	var userTickets uint
+	// ask the user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Printf("Enter your email address: ")
+	fmt.Scan(&emailAddress)
+	fmt.Println("Enter number of Tickets: ")
+	fmt.Scan(&userTickets)
+	return firstName, lastName, emailAddress, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, emailAddress string) {
+	remainingTickets = remainingTickets - userTickets
+
+	//  creating a map i.e dictionary
+	//  map creation is done with that make
+	// maps in go is limited to only one data type
+
+	// var userData = make(map[string]string)
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		emailAddress:    emailAddress,
+		numberOfTickets: userTickets,
+	}
+	// Map example
+	// userData["firstName"] = firstName
+	// userData["lastName"] = lastName
+	// userData["emailAddress"] = emailAddress
+	// userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, emailAddress)
+	fmt.Printf("%v tickets remaining for the %v are left\n", remainingTickets, conferenceName)
 }
